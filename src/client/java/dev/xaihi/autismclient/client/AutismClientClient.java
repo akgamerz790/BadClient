f808154b34dbc408ae31cab14fe4e5de7b0c0584
akgamerz790 <rockdgyt352@gmail.com>
2026-05-27 05:32:07 +0530

    Commited

---
+++ src/client/java/dev/xaihi/autismclient/client/AutismClientClient.java
@@ -0,0 +1,10 @@
+package dev.xaihi.autismclient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutismClientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
